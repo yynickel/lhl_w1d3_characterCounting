c095e8ada28850608415fd1cfcedc96992003e9f
Nick Yu <yynickel@gmail.com>
2019-05-29 18:13:57 +0000

    countLetters implemented

---
+++ characterCounting.js
@@ -0,0 +1,13 @@
+const countLetters = function(str) {
+  str = str.split(" ").join("");
+  let result = {};
+  for (let i = 0; i < str.length; i++) {
+    if (result[str[i]])
+      result[str[i]]++;
+    else
+      result[str[i]] = 1;
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
