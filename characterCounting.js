a67612dd8a1847f6941d5741f98a7a8f05bb6469
Nick Yu <yynickel@gmail.com>
2019-05-29 18:22:02 +0000

    character positioning function implemented

--- characterCounting.js
+++ characterCounting.js
@@ -10,4 +10,16 @@ const countLetters = function(str) {
   return result;
 };
 
+const positionLetters = function(str) {
+  str = str.split(" ").join("");
+  let result = {};
+  for (let i = 0; i < str.length; i++) {
+    if (!result[str[i]])
+      result[str[i]] = [];
+    result[str[i]].push(i);
+  }
+  return result;
+}
+
 console.log(countLetters("lighthouse in the house"));
+console.log(positionLetters("lighthouse in the house"))
